477ca6b34824b6ea43cda4e3d633d63f9755de1b
frontmobi <frontmobi@gmail.com>
2015-10-14 23:29:43 +0800

    20151014 updated

--- spa01/js/main.js
+++ spa01/js/main.js
@@ -24,7 +24,7 @@ $("#menu-toggle").click(function(e) {
 
 // Scrolls to the selected menu item on the page
 $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[id^="mn-"][href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
             var target = $(this.hash);
