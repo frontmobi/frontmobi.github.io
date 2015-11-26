65c747752c92e4b354b7788759625b9873929854
frontmobi <frontmobi@gmail.com>
2015-11-26 21:44:00 +0800

    changed paths of some js and png files

---
+++ static/spa01/js/spa01.js
@@ -0,0 +1,62 @@
+// loader 
+$(window).load(function() {
+    // makes sure the whole site is loaded
+    $("#status").fadeOut();
+    // will first fade out the loading animation
+    $("#preloader").delay(350).fadeOut("slow");
+    // will fade out the white DIV that covers the website.
+    $("body").delay(350).css({
+        overflow: "visible"
+    });
+});
+
+function closeNav() {
+    $("#menu-close").click();
+}
+
+// closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// scrolls to the selected menu item on the page
+$(function() {
+    $('a[id^="mn-"][href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    // activate owl-carousel
+    $('#services-list').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                autoplay: true
+            },
+            767:{
+                items:4,
+                nav:false
+            }
+        }
+    });
+});
