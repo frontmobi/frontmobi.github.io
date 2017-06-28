05f905bce8f813592f5a798ead6b2bf824c6a0c7
frontmobi <frontmobi@gmail.com>
2017-06-28 08:54:45 +0800

    modify spa03

--- static/spa03/js/spa03.js
+++ static/spa03/js/spa03.js
@@ -1,6 +1,13 @@
-/*  preloader */
-jQuery(window).load(function() {
-    $("#preloader").fadeOut("slow");
+// loader 
+$(window).load(function() {
+    // makes sure the whole site is loaded
+    $("#status").fadeOut();
+    // will first fade out the loading animation
+    $("#preloader").delay(350).fadeOut("slow");
+    // will fade out the white DIV that covers the website.
+    $("body").delay(350).css({
+        overflow: "visible"
+    });
 });
 
 /*  welcome section slider */
